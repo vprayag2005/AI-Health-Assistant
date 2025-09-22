b0218d4df73977829ec2b019cd17c5423945230b
Prayag V <v.prayag2005@gmail.com>
2025-09-22 17:27:16 +0530

    removed api key

--- ai/symptom_checker.py
+++ ai/symptom_checker.py
@@ -5,7 +5,7 @@ import streamlit as st
 from transformers import pipeline
 import google.generativeai as genai
 
-API_KEY = "AIzaSyCBHIzA13fNBJ-TetWCzy-r6WOcRRekBOw"
+API_KEY = "#"
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
